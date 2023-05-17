cdc2e4832688249dc83d3b47b68127c71dbe133d
okeken <okekehinde@gmail.com>
2023-05-17 23:26:57 +0100

    homepage

--- src/components/primitives/button.tsx
+++ src/components/primitives/button.tsx
@@ -1,16 +1,16 @@
 import ConnectButton from '@components/ConnectButton';
-// import { useAccount } from "wagmi";
+import { useAccount } from 'wagmi';
 
 const buttonType = {
   solid: 'bg-safekeep-blue w-ull text-safekeep-white p-2 py-3 px-6 rounded-[8px] [&>*]:font-paralucentMedium',
   outline: 'border border-safekeep-blue text-safekeep-blue px-6  wfull  p-2 py-3 rounded-[8px]',
 };
 const Button = ({ children, type = 'solid', className = '', onlyBtn = false, ...props }) => {
-  // const { isConnected } = useAccount();
+  const { isConnected } = useAccount();
 
-  // if (!isConnected && !onlyBtn) {
-  //   return <ConnectButton />;
-  // }
+  if (!isConnected && !onlyBtn) {
+    return <ConnectButton />;
+  }
   return (
     <>
       <button {...props} className={`${buttonType[type]} ${className}  `}>
